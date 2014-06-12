94bf0e7ede5975f176af98694cdd74aa2213755e
Chih-Yu Huang <flarehunter@storysensecomputing.com>
2014-06-12 17:10:03 +0800

    Implement headline.

--- pttdaily.py
+++ pttdaily.py
@@ -12,7 +12,7 @@ def generate_result(form):
     content = form['content']
 
     ret = ''
-    ret += style.header(author, number, date)
+    ret += style.header(author, number, date, headline)
     for line in content.split('\n'):
         if line[:2] == '##':
             line = style.h2(line[2:])
@@ -21,32 +21,6 @@ def generate_result(form):
         ret += line
     ret += style.footer()
 
-    print 'type(ret):', type(ret)
     return ret
 
 
-
-#def main(argv=sys.argv[:]):
-#    if len(argv) <= 2:
-#        return 'Usage: {0} <input> <output>'.format(argv[0])
-#    infile, outfile = argv[1:]
-#
-#    author = 'TestAuthor' 
-#    number = 14
-#    with open(infile, 'r') as fin:
-#        fout = open(outfile, 'w')
-#        fout.write(style.header(author, number))
-#        for line in fin:
-#            if line[:2] == '##':
-#                line = style.h2(line[2:])
-#            elif line[:1] == '#':
-#                line = style.h1(line[1:])
-#            fout.write(line)
-#
-#        fout.write(style.footer())
-#
-#    return 0
-#
-#if __name__ == '__main__':
-#    sys.exit(main())
-#
--- style.py
+++ style.py
@@ -2,62 +2,63 @@
 # -*- coding: utf-8 -*-
 """Implement the style function."""
 
-from time import strftime
-from ansi import ESC
-
 def h1(string):
     string = string.strip()
-    return '{0}[46m  {0}[47m {0}[30m{1}  {0}[m\n'.format(ESC, string)
+    return '\025[46m  \025[47m \025[30m{0}  \025[m\n'.format(string)
 
 def h2(string):
     string = string.strip()
-    return '{0}[1;36m◎{1}{0}[m\n'.format(ESC, string)
+    return '\025[1;36m◎{0}\025[m\n'.format(string)
 
 def fill_string(string, length):
     """Fill space until the length.
     Ex: fill_string('abc', 5)  => 'abc  '
         fill_string('你好', 5) => '你好 ' 
-        fill_string('啦啦啦超過啦', 4) => Exception XDD
+        fill_string('啦啦啦超過啦', 4) => '啦啦'
     """
+    string = string.strip()
     current_length = len(string.decode('utf-8').encode('big5'))
-    return string + ' ' * (length - current_length)
+    if length > current_length:
+        return string + ' ' * (length - current_length)
+    else:
+        return string
 
-def header(author, number, date):
+def header(author, number, date, headline):
     """ptt daily首頁."""
-    ret = '''
-{0}[m
-{0}[0;1;30m  ██￣╲ ￣██￣ ￣██￣   ██￣╲          ￣￣ ██{0}[m
-{0}[0;1;30m  ██  ▕   ██     ██     ██  ▕ ╱￣█◣ ██ ██ ▏  ██{0}[m
-{0}[0;1m  ██ˍ╱   ██     ██     ██  ▕ ╱￣██ ██ ██ ▏  ██{0}[m
-  {0}[0;1m██       ██     ██     ██ˍ╱ ╲ˍ██ ██ ██ ╲ˍ██ #{3}{0}[m
-                                                           {0}[0;1m╲ˍ█◤           {0}[0m　{0}[m
-  {1}ψ{2}{0}[m
-{0}[m
-   {0}[41m                                                                {0}[m
-   {0}[1;41m  ◆立法院臨時會，倒數四天！                                    {0}[m
-   {0}[41m                                                                {0}[m
-  {0}[0;31m┌───────────────────────────────┐{0}[m
-  {0}[0;31m│                                                              │{0}[m
-  {0}[0;31m│ {0}[0;1;31m立院臨時會 馬下令：過服貿、擋投票年齡下修 {0}[0m
-  {0}[0;31m│                                                              │{0}[m
-  {0}[0;31m│ {0}[0;1;31m臨時會前 民進黨擴大會議凝聚共識 {0}[0mhttp://ppt.cc/-idZ
-  {0}[0;31m│                                                              │{0}[m
-  {0}[0;31m│ {0}[0m閉門會議將針對：考、監兩院人事同意權                         {0}[m
-  {0}[0;31m│ {0}[0m兩岸協議監督條例，及兩岸服務貿易協議等4大議題交換意見，      {0}[m
-  {0}[0;31m│ {0}[0m凝聚黨內共識，並討論相關議事攻防。                           {0}[m
-  {0}[0;31m│                                                              │{0}[m
-  {0}[0;31m└───────────────────────────────┘{0}[m
+    COUNT = 59
+    headlines = headline.split('\n')
 
-'''.format(ESC, date, author, number)
+    ret = '''
+\025[m
+\025[0;1;30m  ██￣╲ ￣██￣ ￣██￣   ██￣╲          ￣￣ ██\025[m
+\025[0;1;30m  ██  ▕   ██     ██     ██  ▕ ╱￣█◣ ██ ██ ▏  ██\025[m
+\025[0;1m  ██ˍ╱   ██     ██     ██  ▕ ╱￣██ ██ ██ ▏  ██\025[m
+\025[0;1m  ██       ██     ██     ██ˍ╱ ╲ˍ██ ██ ██ ╲ˍ██ #{2}\025[m
+                                                           \025[0;1m╲ˍ█◤           \025[0m　\025[m
+  \025[m{0}ψ{1}\025[m
+\025[m
+  \025[41m                                                                \025[m
+'''.format(date, author, number)
+    ret += '  \025[1;41m  ◆ {0}\025[m\n'.format(fill_string(headlines[0], COUNT))
+    ret += '''  \025[41m                                                                \025[m
+  \025[0;31m┌───────────────────────────────┐\025[m
+  \025[0;31m│                                                              │\025[m
+'''
+    for line in headlines[1:]:
+        ret += '  \025[0;31m│ \025[0m{0} \025[0;31m │\025[m\n'.format(fill_string(line, COUNT))
+    ret += '''  \025[0;31m│                                                              │\025[m
+  \025[0;31m└───────────────────────────────┘\025[m
+
+'''
     return ret
 
 def footer():
     """Ptt Daily 頁尾"""
     ret = '''
-{0}[m
-  {0}[47m                {0}[46m                                                  {0}[m
-  {0}[30;47m  【鄉民日報】  {0}[1;37;46m   做個活活潑潑的好鄉民，當個堂堂正正的台灣人。   {0}[m
-  {0}[47m                {0}[46m                                                  {0}[m
-{0}[m
-'''.format(ESC)
+\025[m
+  \025[47m                \025[46m                                                  \025[m
+  \025[30;47m  【鄉民日報】  \025[1;37;46m   做個活活潑潑的好鄉民，當個堂堂正正的台灣人。   \025[m
+  \025[47m                \025[46m                                                  \025[m
+\025[m
+'''
     return ret
