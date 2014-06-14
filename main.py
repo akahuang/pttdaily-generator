e7dd924a008431c97d72a225c66ee2c73a4017ae
Chih-Yu Huang <flarehunter@storysensecomputing.com>
2014-06-14 22:02:09 +0800

    Add default values.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 import traceback
 from collections import defaultdict
 from flask import Flask, request, render_template, make_response
@@ -11,9 +12,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def main():
-    if request.form.get('action') is None:
-        return render_template('index.html', request=defaultdict(str), result='')
     try:
+        if request.form.get('action') is None:
+            default_value = defaultdict(str)
+            default_value['author'] = 'meowmeowgo'
+            default_value['date'] = datetime.today().strftime('%Y.%m.%d')
+            default_value['number'] = default_number(datetime.today())
+            default_value['content'] = default_title(['活動', '社會', '政治', '公民', '國際', '溫馨', '娛樂', '八卦'])
+            return render_template('index.html', request=default_value, result='')
+
         form = {
             'author'   : request.form.get('author', '').encode('utf-8'),
             'date'     : request.form.get('date', '').encode('utf-8'),
@@ -54,3 +61,11 @@ def update_from_cookie_and_post(var, key):
         var = request.form.get(key).encode('utf-8')
     return var
 
+
+def default_title(title_list):
+    return ''.join(map(lambda s: '# ' + s + '版\n\n\n', title_list)).decode('utf-8')
+
+def default_number(current_date):
+    """The first date is 2014.05.30."""
+    ZERO_DATE = datetime.strptime('2014.05.29', '%Y.%m.%d')
+    return (current_date - ZERO_DATE).days
